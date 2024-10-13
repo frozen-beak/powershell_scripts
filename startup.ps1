d53553425770d0c9f5118783b5f015838c03bd14
adii <77727680+frozen-beak@users.noreply.github.com>
2024-10-13 10:45:44 +0530

    updated script order

--- startup.ps1
+++ startup.ps1
@@ -5,7 +5,7 @@
 Start-Sleep 10
 
 # Load and execute each script in order
-C:\dev\powershell_scripts\scripts\wallpaper.ps1
-C:\dev\powershell_scripts\scripts\terminal.ps1
 C:\dev\powershell_scripts\scripts\vsc.ps1
+C:\dev\powershell_scripts\scripts\terminal.ps1
 C:\dev\powershell_scripts\scripts\vsc_theme.ps1
+C:\dev\powershell_scripts\scripts\wallpaper.ps1
